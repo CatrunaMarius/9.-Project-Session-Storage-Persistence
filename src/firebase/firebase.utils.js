ff083344792d213387f0247b6aa060dc75367060
Marius <roberto_marius12@yahoo.com>
2020-10-19 19:33:35 +0300

    implemented firebase utils

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -17,6 +17,8 @@ const firebaseConfig = {
 
   // Initialize Firebase
   firebase.initializeApp(firebaseConfig);
+
+  
   
 
   export const auth = firebase.auth();
@@ -26,20 +28,81 @@ const firebaseConfig = {
 const provider = new firebase.auth.GoogleAuthProvider();
 
 // provider.setCustomParameters({ prompt: 'select_accont' });
-provider.setCustomParameters({
-  'login_hint': 'user@example.com'
-});
+// provider.setCustomParameters({
+//   'login_hint': 'user@example.com'
+// });
 
-// export const signInWithGoogle = () => auth.signInWithPopup(provider);
+// export const signInWithGoogle = () => {auth.signInWithPopup(provider);}
 
 export const signInWithGoogle = () => firebase.auth().signInWithRedirect(provider);
-// firebase.auth().signInWithPopup(provider)
-// .then(function(result) {
-//   // This gives you a Google Access Token.
-//   var token = result.credential.accessToken;
-//   // The signed-in user info.
-//   var user = result.user;
-//  });
+
+// initialize database
+  //preaia userul autetificat cu google si il trece in database
+  export const createUserProfileDocument = async (userAuth, additionalData ) => {
+    if (!userAuth) return;
+
+    
+    const userRef = firestore.doc(`users/${userAuth.uid}`);
+
+    const snapShot = await userRef.get();
+
+    // daca nu exista vom crea acel utilizator in baza de date
+    //verifica daca acel user exita
+    if (!snapShot.exists){
+      const { displyName, email } = userAuth;
+      const createdAt = new Date();
+
+      // creaza acel user daca nu exista
+      try {
+        await userRef.set({
+          displyName,
+          email,
+          createdAt,
+          ...additionalData,
+        });
+      } catch (error) {
+        console.log('error create user ', error.message);
+      }
+
+    }
+    
+    return userRef;
+  }
+
+ 
+
+
+  // export const createUserProfileDocument = async (user, additionalData) => {
+  //   if (!user) return;
+  //   const userRef = firestore.doc(`users/${user.uid}`);
+  //   const snapshot = await userRef.get();
+  //   if (!snapshot.exists) {
+  //     const { email, displayName, photoURL } = user;
+  //     try {
+  //       await userRef.set({
+  //         displayName,
+  //         email,
+  //         photoURL,
+  //         ...additionalData
+  //       });
+  //     } catch (error) {
+  //       console.error("Error creating user document", error);
+  //     }
+  //   }
+  //   return getUserDocument(user.uid);
+  // };
+  // const getUserDocument = async uid => {
+  //   if (!uid) return null;
+  //   try {
+  //     const userDocument = await firestore.doc(`users/${uid}`).get();
+  //     return {
+  //       uid,
+  //       ...userDocument.data()
+  //     };
+  //   } catch (error) {
+  //     console.error("Error fetching user", error);
+  //   }
+  // };
 
 
 export default firebase;
