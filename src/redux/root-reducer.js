fa1fb674bf210928b213e084a2cef98b4298e4fc
Marius <roberto_marius12@yahoo.com>
2020-10-27 14:19:42 +0200

    Storage Cart local

--- src/redux/root-reducer.js
+++ src/redux/root-reducer.js
@@ -1,9 +1,21 @@
 import { combineReducers } from 'redux';
+import { persistReducer} from 'redux-persist';
+import storage  from 'redux-persist/lib/storage'
+
 
 import userReducer from './user/user.reducer';
 import cartReaducer from './cart/cart.reducer';
 
-export default combineReducers ({
+// stocare locala
+const persistConfig ={
+    key: 'root',
+    storage,
+    whitelist: ['cart']
+}
+
+const rootReducer = combineReducers ({
     user: userReducer,
     cart: cartReaducer
 })
+
+export default persistReducer(persistConfig, rootReducer);
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,10 +1,15 @@
 import { createStore, applyMiddleware } from 'redux';
+import { persistStore } from 'redux-persist';
 import logger from 'redux-logger';
 
 import rootReaducer from './root-reducer';
 
 const middlewares = [logger];
 
-const store = createStore(rootReaducer, applyMiddleware(...middlewares));
+// stocare locala
+export const store = createStore(rootReaducer, applyMiddleware(...middlewares));
 
-export default store
+export const persistor = persistStore(store);
+//===============================================
+
+export default {store, persistor}
